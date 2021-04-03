9073edcf7fd92ddd5db500fbf988253130f7c552
DeusPio <61561634+DeusPio@users.noreply.github.com>
2021-04-03 04:23:20 -0400

    LoginToDashBoard

--- src/main/java/com/TeamProject/Controller/LoginController.java
+++ src/main/java/com/TeamProject/Controller/LoginController.java
@@ -1,14 +1,27 @@
 package com.TeamProject.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
     @RequestMapping("/user/login")
-    @ResponseBody
-    public String login(String username, String password){
-        return "Username: " + username + " Password: " + password;
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Model model, HttpSession session){
+
+        //need database
+        if("123456".equals(password)){
+            session.setAttribute("loginUser",username);
+            return "redirect:/main";
+        }else{
+            model.addAttribute("msg","wrong password");
+            return "index";
+        }
     }
 }
--- src/main/java/com/TeamProject/config/MvcConfig.java
+++ src/main/java/com/TeamProject/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.TeamProject.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,12 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main").setViewName("dashboard");
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns("/index.html","/","/user/login","/img/**","/css/*");
     }
 }
