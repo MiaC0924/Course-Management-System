7f286b2eb4d1821ee65d4122221aba261682c698
DeusPio <61561634+DeusPio@users.noreply.github.com>
2021-04-06 23:44:12 -0400

    fin

--- src/main/java/com/TeamProject/Dao/StudentDao.java
+++ src/main/java/com/TeamProject/Dao/StudentDao.java
@@ -1,8 +1,12 @@
 package com.TeamProject.Dao;
 
 import com.TeamProject.Person.Student;
+import com.mongodb.client.result.UpdateResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,8 +15,38 @@ public class StudentDao {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    public void addStudent(Student stu){
-        mongoTemplate.save(stu);
+    public boolean addStudent(Student stu){
+        if(findStudentByEmail(stu.getAddress())!= null){
+            return false;
+        }
+        mongoTemplate.save(stu)
+        return true;
+    }
+    public Student findStudentByEmail(String email){
+        if(email == null){
+            return null;
+        }
+        Query query = new Query((Criteria.where("address").is(email)));
+        Student stu = mongoTemplate.findOne(query , Student.class);
+        return stu;
+    }
+
+    public boolean updateStudentMajorGPAByEmail(String email,Double majorGPA){
+        Query query = new Query(Criteria.where("address").is(email));
+        Update update = new Update().set("majorGPA",majorGPA);
+
+        UpdateResult result = mongoTemplate.updateFirst(query,update,Student.class);
+
+        if(result != null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void deleteStudentByEmail(String email){
+        Query query = new Query(Criteria.where("address").is(email));
+        mongoTemplate.remove(query,Student.class);
     }
 
 }
