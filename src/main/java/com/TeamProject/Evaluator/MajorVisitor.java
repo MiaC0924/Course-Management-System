9c3abf3a8b1779842b498c539f5b9bf6d21d8b6b
Mia Chen <64107453+MiaC0924@users.noreply.github.com>
2021-04-07 15:48:27 -0400

    fix visitor after change on student, add student major visitor logic (#11)
    fix some error on CourseSection.java

--- src/main/java/com/TeamProject/Evaluator/MajorVisitor.java
+++ src/main/java/com/TeamProject/Evaluator/MajorVisitor.java
@@ -1,7 +1,10 @@
 package com.TeamProject.Evaluator;
 
+import com.TeamProject.Course.CourseSection;
 import com.TeamProject.Person.Professor;
 import com.TeamProject.Person.Student;
+
+import java.util.Map;
 
 public class MajorVisitor implements Visitor{
 
@@ -36,15 +39,43 @@ public class MajorVisitor implements Visitor{
     @Override
     public double visit(Student inputStu) {
         double output = 0.0;
-        int count = 0;
+        int     count = 0;
 
-        //TODO: identify the major courses, and do the calculation
+        if (inputStu.getFinalGrades().isEmpty()){
+            inputStu.setMajorGPA(output);
+            return output;
+        }else {
+            Map<CourseSection,Character> gradeMap = inputStu.getFinalGrades();
 
-        if(count != 0) {
+            for (CourseSection key : gradeMap.keySet()) {
+                if(key.getMajor().equals(inputStu.getMajor())) {
+                    output += gradeCalculation(gradeMap.get(key));
+                    ++count;
+                }
+            }
+
             output = output / count;
+
+            //means this student has all course fail
+            if(count == 0){
+                output = -1.0;
+            }
         }
 
         inputStu.setMajorGPA(output);
         return output;
+    }
+
+    public double gradeCalculation(Character grade){
+        if (grade == 'A') {
+            return 4.0;
+        } else if (grade == 'B') {
+            return 3.0;
+        } else if (grade == 'C') {
+            return 2.0;
+        } else if (grade == 'D') {
+            return 1.0;
+        }
+        return 0.0;
     }
 }
