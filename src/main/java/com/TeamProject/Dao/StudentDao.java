5a4ae811e9a5b004f2a79f399eb054fc63b8aed1
Xiangfei Kong <61561634+DeusPio@users.noreply.github.com>
2021-04-08 15:05:48 +0800

    Student db xiangfei (#14)

--- src/main/java/com/TeamProject/Dao/StudentDao.java
+++ src/main/java/com/TeamProject/Dao/StudentDao.java
@@ -1,8 +1,12 @@
 package com.TeamProject.Dao;
 
 import com.TeamProject.Person.Student;
+import com.mongodb.client.result.UpdateResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,8 +15,35 @@ public class StudentDao {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    public void addStudent(Student stu){
+    public boolean addStudent(Student stu){
+        if(findStudentByStuId(stu.getStudentNumber())!= null){
+            return false;
+        }
         mongoTemplate.save(stu);
+        return true;
+    }
+    public Student findStudentByStuId(int id){
+        Query query = new Query((Criteria.where("studentNumber").is(id)));
+        Student stu = mongoTemplate.findOne(query , Student.class);
+        return stu;
+    }
+
+    public boolean updateStudentMajorGPAByStuId(int id,Double majorGPA){
+        Query query = new Query(Criteria.where("studentNumber").is(id));
+        Update update = new Update().set("majorGPA",majorGPA);
+
+        UpdateResult result = mongoTemplate.updateFirst(query,update,Student.class);
+
+        if(result != null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void deleteStudentByStuId(int id){
+        Query query = new Query(Criteria.where("studentNumber").is(id));
+        mongoTemplate.remove(query,Student.class);
     }
 
 }
---
+++ src/test/java/com/TeamProject/Dao/StudentDaoSuite.java
@@ -0,0 +1,5 @@
+package com.TeamProject.Dao;
+
+public class StudentDaoSuite {
+
+}
