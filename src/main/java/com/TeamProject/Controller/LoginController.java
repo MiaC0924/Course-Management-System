ee4252f260b00e6d6e641d128dc2ca3ccfdefa13
DeusPio <61561634+DeusPio@users.noreply.github.com>
2021-04-09 04:00:24 -0400

    2

--- src/main/java/com/TeamProject/Controller/LoginController.java
+++ src/main/java/com/TeamProject/Controller/LoginController.java
@@ -37,24 +37,24 @@ public class LoginController {
 
 
 //
-//            Student stu = new Student("Gura2","F",username, LocalDate.now(),"123","MATH");
+         Student stu = new Student("Gura2","F",username, LocalDate.now(),"123","MATH");
 //           // stuDao.addStudent(stu);
 //            //stu = stuDao.findStudentByStuId(stu.getStudentNumber());
 //            System.out.println(stu);
 //           // stuDao.updateStudentMajorGPAByStuId(100000001,100.0);
 //            Professor john = new Professor();
-//            University university = new University("Carleton");
-//            Course comp3004 = university.getDepartments().orderTheCourse("COMP",3004);
-//            //Course comp3004 = new Course("COMP", 3004);
-//            Term winter2021 = new Term(2021, "Winter");
-//            CourseSection comp3004A = new CourseSection(comp3004, 'A', winter2021);
+            University university = new University("Carleton");
+           Course comp3004 = university.getDepartments().orderTheCourse("COMP",3004);
+//            Course comp3004 = new Course("COMP", 3004);
+           Term winter2021 = new Term(2021, "Winter");
+            CourseSection comp3004A = new CourseSection(comp3004, 'A', winter2021);
 //
-//            comp3004A.addStudent(stu);
-//            comp3004A.addGrade(stu,'A');
+            comp3004A.addStudent(stu);
+            comp3004A.addGrade(stu,'A');
 //            stu.addCourse(comp3004A);
 //
 //           // stuDao.deleteStudentByStuId(100000001);
-//            stuDao.addStudent(stu);
+            stuDao.addStudent(stu);
            //stuDao.updateStudentCourseSectionByStuId(100000001, comp3004A);
             return "redirect:/"+ btnradio + "/main";
         }else{
