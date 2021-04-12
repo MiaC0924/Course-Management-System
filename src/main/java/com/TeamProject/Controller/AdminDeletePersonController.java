3c214d2877a3d69ece1f18dc7afdf206b6439902
Xiangfei Kong <61561634+DeusPio@users.noreply.github.com>
2021-04-13 01:02:31 +0800

    create+delete_person @admin (#41)

---
+++ src/main/java/com/TeamProject/Controller/AdminDeletePersonController.java
@@ -0,0 +1,55 @@
+package com.TeamProject.Controller;
+
+import com.TeamProject.Person.Admin;
+import com.TeamProject.Service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AdminDeletePersonController {
+    @Autowired
+    AdminService adminService;
+
+    @RequestMapping("/Admin/Person/Delete/Processing")
+    public String deleteCourse(@RequestParam("btnradio") String btnradio,
+                               @RequestParam("id") int id,
+                               @RequestParam("email") String email,
+                               Model model){
+        boolean flag1 = false;
+        System.out.println("Want to delete: " + btnradio + " id: " + id + " email: " + email);
+
+
+        if(btnradio.equals("Professor")){
+            //bug !!!   adminService.deleteProfessor(id,email)  !!! //
+            if(adminService.deleteProfessor(id,email)){
+                flag1 = true;
+            }else{
+                flag1 = false;
+            }
+
+        }else if (btnradio.equals("Student")){
+            //bug !!!   adminService.deleteStudent(id)  !!! //
+            if(adminService.deleteStudent(id)){
+                flag1 = true;
+            }else{
+                flag1 = false;
+            }
+        }
+
+
+
+
+        if(flag1){
+            System.out.println("delete success");
+            model.addAttribute("msg1" ,"success");
+        }else{
+            System.out.println("delete course fail");
+            model.addAttribute("msg1", "fail");
+        }
+        return "dashboardAdm";
+    }
+
+}
