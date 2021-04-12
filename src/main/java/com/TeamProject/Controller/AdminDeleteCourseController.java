bdfe63c0e0249d75ac0c6f28909431b32504d0e1
DeusPio <61561634+DeusPio@users.noreply.github.com>
2021-04-12 08:59:50 -0400

    fin

--- src/main/java/com/TeamProject/Controller/AdminDeleteCourseController.java
+++ src/main/java/com/TeamProject/Controller/AdminDeleteCourseController.java
@@ -1,46 +1,33 @@
 package com.TeamProject.Controller;
 
-import com.TeamProject.Course.Course;
-import com.TeamProject.Course.CourseSection;
-import com.TeamProject.Course.Term;
-import com.TeamProject.Course.University;
-import com.TeamProject.Dao.CourseDao;
-import com.TeamProject.Dao.CourseSectionDao;
+import com.TeamProject.Person.Admin;
+import com.TeamProject.Service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminDeleteCourseController {
-    // @Autowired a courseSection service
+    @Autowired
+    AdminService adminService;
 
-
-    @RequestMapping("Admin/Courses/Delete")
-    public String DeleteCourse(Model model){
-        ArrayList<CourseSection> csList = new ArrayList<CourseSection>();
-       //find all CourseSection
-
-        /*          Test Area                       */
-        University university = new University("Carleton");
-        Course comp3004 = university.getDepartments().orderTheCourse("COMP",3004);
-        Course comp3005 = university.getDepartments().orderTheCourse("COMP",3005);
-        Term winter2021 = new Term(2021, "Winter");
-        CourseSection comp3004A = new CourseSection(comp3004, 'A', winter2021);
-        CourseSection comp3004B = new CourseSection(comp3004, 'B', winter2021);
-        CourseSection comp3005A = new CourseSection(comp3005, 'A', winter2021);
-
-        csList.add(comp3004A);
-        csList.add(comp3004B);
-        csList.add(comp3005A);
-
-
-
-
-        /*             Test Area                  */
-        model.addAttribute("courses" , csList);
-        return "deleteCourseAdmin";
+    @RequestMapping("/Admin/Courses/Delete/Processing")
+    public String deleteCourse(@RequestParam("id") int id,
+                               @RequestParam("major") String major,
+                               @RequestParam("code") int code,
+                               @RequestParam("section") char section,
+                               Model model){
+        System.out.println("Want to delete: Major: " + major + " Code: " + code + " section: " + section );
+        if(/*adminService.deleteCourse(id, major,code,section)*/ true){
+                System.out.println("delete success");
+                model.addAttribute("msg1" ,"success");
+        }else{
+            System.out.println("delete course fail");
+            model.addAttribute("msg1", "fail");
+        }
+        return "dashboardAdm";
     }
+
 }
